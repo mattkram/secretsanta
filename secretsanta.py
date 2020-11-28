ee073baf871855a3cab80ad22eee8c856f079fea
Matt Kramer <matthew.robert.kramer@gmail.com>
2020-11-28 15:37:32 -0600

    Clean up

--- secretsanta.py
+++ secretsanta.py
@@ -1,29 +1,27 @@
 import random
 
 
-def distribute_gifts(partners, last_year):
-    # Flatten partner dictionary into a list of non-blank
-    givers =  [person for pair in partners.items() for person in pair if person is not None]
+def distribute_gifts(sibling_partner_map, last_year):
 
-    new_partners = dict(partners)
-    for sibling, partner in partners.items():
+    partners = dict(sibling_partner_map)
+    for sibling, partner in sibling_partner_map.items():
         if partner is not None:
-            new_partners[partner] = sibling
-    partners = new_partners
+            partners[partner] = sibling
+
+    givers = partners.keys()
 
     while True:
         receivers = sorted(givers, key=lambda _: random.random())
         g_r_dict = dict(zip(givers, receivers))
 
         if all(
-            giver != receiver and
-            receiver != partners.get(giver) and
-            giver != partners.get(receiver) and
-            receiver != last_year[giver] and
-            g_r_dict[receiver] != giver
+            giver != receiver and                 # a person cannot give to themself
+            receiver != partners.get(giver) and   # a person cannot give to their partner
+            receiver not in last_year[giver] and  # a person cannot give to the same person as in the last however many years
+            g_r_dict[receiver] != giver           # two people cannot give to each other
             for giver, receiver in g_r_dict.items()
         ):
-            return zip(givers, receivers)
+            return g_r_dict
 
 # Sibling: Partner
 partner = {
@@ -35,16 +33,16 @@ partner = {
 
 # Giver: Receiver
 last_year = {
-    'Matt': 'Tim',
-    'Amanda': 'Kelly',
-    'Marianne': 'Megan',
-    'Megan': 'Marianne',
-    'Tim': 'Viral',
-    'Kelly': 'Amanda',
-    'Viral': 'Matt',
+    'Matt': ['Tim'],
+    'Amanda': ['Kelly'],
+    'Marianne': ['Megan'],
+    'Megan': ['Marianne'],
+    'Tim': ['Viral'],
+    'Kelly': ['Amanda'],
+    'Viral': ['Matt'],
 }
 
 results = distribute_gifts(partner, last_year)
 
-for giver, receiver in results:
+for giver, receiver in results.items():
     print(giver, '->', receiver)
